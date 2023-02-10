4f367397d3131e1a28dee387bc76896b2dbadc9e
nikhil-mallabadi <125029573+nikhil-mallabadi@users.noreply.github.com>
2023-02-10 17:12:28 +0530

    Add files via upload
    Adding files to FirstProject

---
+++ GradleSeleniumProject/untitled/src/test/java/Libraries/AuthenticationFunctions.java
@@ -0,0 +1,17 @@
+package Libraries;
+
+import Elements.WebELements;
+
+public class AuthenticationFunctions {
+    WebELements elements;
+    {
+        elements = new WebELements(DriverSetup.webdriver);
+    }
+    public AuthenticationFunctions(){
+
+    }
+
+    public void enterEmail(){
+        elements.loginButton.click();
+    }
+}
